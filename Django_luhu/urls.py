0ae20e4db0abf21f3e3ea8793e40f9b62f8600d4
luhu <lu.hu@xinheyun.com>
2018-09-21 19:35:52 +0800

    初始化

--- Django_luhu/urls.py
+++ Django_luhu/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', views.blog_index),
 ]
